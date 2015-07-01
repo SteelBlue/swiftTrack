22625d709edd19fc25bf9364b958fc186ecf688a
Ryan Ebbers <info@ryanebbers.com>
2015-07-01 18:04:47 -0400

    completed NumbersBooleans lecture, finished Types Section

--- swiftBasics/NumbersBooleans.playground/Contents.swift
+++ swiftBasics/NumbersBooleans.playground/Contents.swift
@@ -2,4 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+/*
+
+Integers
+  - used for whole numbers, numbers without decimals
+  - not surrounded by quotes like a String
+
+Double
+  - used for fractional numbers, numbers with decimal places
+  - up to 15 decimal digits
+  - more precise than Floats
+
+Float
+  - used for fractional numbers
+  - up to 7 decimal digits
+
+Bool
+  - only two values (true - false)
+
+*/
+
+let language : String = "Swift"
+
+let year : Int = 2014 // Int or Integer
+
+var version : Double = 1.0 // Double
+
+var isAwesome : Bool = true // Boolean or Bool
