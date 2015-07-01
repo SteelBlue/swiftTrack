d16eb608d4dd33e1b7658373b65e0cabbbd97e25
Ryan Ebbers <info@ryanebbers.com>
2015-07-01 17:34:23 -0400

    added string comments

--- swiftBasics/Strings.playground/Contents.swift
+++ swiftBasics/Strings.playground/Contents.swift
@@ -2,4 +2,27 @@
 
 import UIKit
 
-var str = "Hello, playground"
+/*
+
+Mutable String
+  - means a String that 'can be modified'
+  - declared as a variable
+  - var language = "Swift"
+
+Immutable String
+  - means a String that 'cannot be modified'
+  - declared as a constant
+  - let language = "Swift"
+
+Concatenation
+  - added Strings together
+  - use the '+' operator to combine Strings
+  - var greeting = "Hello " + language
+
+String Interpolation
+  - means a variable is being inserted into a string
+  - var greetings = "Hello \(language)"
+
+*/
+
+
