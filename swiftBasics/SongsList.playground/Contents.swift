74570c5940c4e352130e7c6c08ed00ae1f6ac06a
Ryan Ebbers <info@ryanebbers.com>
2015-07-02 12:20:29 -0400

    completed SongList extra credit

---
+++ swiftBasics/SongsList.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - SongsList
+
+import UIKit
+
+// Create Array for Songs
+var playlist = []
+
+let song1 = [
+              "title"  : "No Miracles",
+              "artist" : "Kid Ink",
+              "album"  : "My Own Lane"
+            ]
+let song2 = [
+              "title"  : "See You Again",
+              "artist" : "Wiz Khalifa",
+              "album"  : "Furious 7: Soundtrack"
+            ]
+let song3 = [
+              "title"  : "Energy",
+              "artist" : "Drake",
+              "album"  : "If You're Reading This It's Too Late"
+            ]
+let song4 = [
+              "title"  : "F.A.M.E.",
+              "artist" : "Jeezy, T.I.",
+              "album"  : "F.A.M.E."
+            ]
+let song5 = [
+              "title"  : "Perfect Imperfection",
+              "artist" : "Kevin Gates",
+              "album"  : "Luca Brasi 2: Gangsta Grillz"
+            ]
+
+playlist = [song1, song2, song3, song4, song5]
+print(playlist)
