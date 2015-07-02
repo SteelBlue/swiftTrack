c38b6661e26f90d8256040ef0ac8b027b2e581f5
Ryan Ebbers <info@ryanebbers.com>
2015-07-02 16:35:43 -0400

    completed CreateFunction lecture

---
+++ swiftFunctionsOptionals/CreateFunction.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - Creating a Function
+
+import UIKit
+
+/*
+
+Functions
+  - self-contained chunks of code that perform a specific task
+
+*/
+
+println("Swift Functions")
+
+func calculateArea() {
+  let height = 12
+  let width = 10
+  let area = height * width
+  println("The area of the room is \(area)")
+}
+
+calculateArea()
