5196c3ea7bcb97c7ad124be46f5e292623751f64
Ryan Ebbers <info@ryanebbers.com>
2015-07-02 15:16:54 -0400

    completed IfStatement lecture, started SwitchStatement lecture

--- swiftBasics/IfStatement.playground/Contents.swift
+++ swiftBasics/IfStatement.playground/Contents.swift
@@ -1,5 +1,44 @@
-//: Playground - noun: a place where people can play
+//: Playground - If Statement
 
 import UIKit
 
-var str = "Hello, playground"
+/*
+
+Control Flow Statements
+  - two kinds of statements
+    - loops
+    - conditionals
+
+If/Else Statements
+  - a fork in the road
+  - if true do this, else do this
+  - else condition is always last
+
+Else If Statements
+  - test another condition within and if statement
+
+Pseudo Code
+  if on train
+    read book
+  else if in car
+    listen to music
+  else
+    play game
+
+*/
+
+let cards = 1...13
+
+for card in cards {
+  if card == 11 {
+    println("Jack")
+  } else if card == 12 {
+    println("Queen")
+  } else if card == 13 {
+    println("King")
+  } else {
+    println(card)
+  }
+}
+
+
