9d26d7ea169f08ffdc9a8841d0308b4f1c5f8b69
Ryan Ebbers <info@ryanebbers.com>
2015-07-02 10:18:26 -0400

    completed OperatorPrecedence lecture

---
+++ swiftBasics/OperatorPrecedence.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - Operator Precedence
+
+import UIKit
+
+/*
+
+Operator Precedence Levels
+  - Level 150
+    - multiplication
+    - division
+    - remainder
+  - Level 140
+    - addition
+    - subtraction
+
+Operator Precedence
+  - if same precedence level it works left -> right
+
+*/
+
+var x = 100 + 100 - (((5 * 2) / 3) % 7)
+
+
