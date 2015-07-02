9ca4c87d9bef791569f59bee7642a6a6ab85eb3d
Ryan Ebbers <info@ryanebbers.com>
2015-07-02 17:36:11 -0400

    completed NamedTuples lecture

--- swiftFunctionsOptionals/NamedTuples.playground/Contents.swift
+++ swiftFunctionsOptionals/NamedTuples.playground/Contents.swift
@@ -11,14 +11,19 @@ Named Tuples
   - safer way to access values
 
 Access Named Tuple Values
-  - preferred way to access Tuple values
+  - return value to a Tuple with named constants
   - names allow for the correct value to be used at runtime
   - assign the returned Tuple to a Tuple
   - use an underscore if the Tuple value is not needed
 
+Name Tuple Return Values
+  - name the Tuple Values within the return of the function
+  - able to assign returned Tuple to a single variable/constant
+  - use the name in dot notation to display the value
+
 */
 
-func searchNames(#name: String) -> (Bool, String) {
+func searchNames(#name: String) -> (found: Bool, description: String) {
   let names = ["Amit", "Andrew", "Ben", "Craig", "Dave", "Guil", "Hampton", "Jason", "Joy", "Kenneth", "Nick", "Pasan", "Zac"]
   
   var found = (false, "\(name) is not a Treehouse Teacher")
@@ -32,11 +37,17 @@ func searchNames(#name: String) -> (Bool, String) {
   return found
 }
 
-let (_, description) = searchNames(name: "Amit")
 
-description
+// Only store one value in the Tuple
+let (_, description1) = searchNames(name: "Amit")
+description1
 
-let (found, description2) = searchNames(name: "Jon")
+// Store both values in the Tuple
+let (found1, description2) = searchNames(name: "Jon")
+found1
+description2
 
-found
-description
+// Store within a variable/constant
+let results = searchNames(name: "Andrew")
+results.found
+results.description
